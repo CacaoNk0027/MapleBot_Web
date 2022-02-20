6be9b2cb3dce4b5199b0a6ec6d1bc3088f9d4069
CacaoNk0027 <jbzca@outlook.es>
2022-02-19 20:04:13 -0600

    make it better

--- src/express/index.js
+++ src/express/index.js
@@ -5,6 +5,14 @@ router.get('/', (req, res) => {
     res.render('pages/welcome.html')
 })
 
+router.get('/terms-of-use', async (req, res) => {
+    res.render('pages/termsOfUse.html')
+})
+
+router.get('/privacy-policy', async (req, res) => {
+    res.render('pages/privacyPolicy.html')
+})
+
 router.use('/commands', require('./routes/commands'))
 
 router.use('/api', require('./routes/api'))
