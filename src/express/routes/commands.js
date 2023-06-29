eb3cedb86f509330617f5269bbfdc9aeb37451f0
CacaoNk0027 <jbzca@outlook.es>
2023-06-29 02:59:49 -0600

    make it better

--- src/express/routes/commands.js
+++ src/express/routes/commands.js
@@ -2,7 +2,8 @@ const { Router } = require('express')
 let router = Router()
 
 router.get('/', (req, res) => {
-    res.render('pages/commands.html')
+    // res.render('pages/commands.html')
+    res.status(503).render('errors/error503.html')
 })
 
 module.exports = router
